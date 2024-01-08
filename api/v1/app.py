f8bfd3b95ccd23f6c04369cced53b8fa456e266d
kibuchijw <kibuchijw@gmail.com>
2024-01-08 07:34:36 +0000

    Add (api/v1/views/states.py), that handles all default RESTFul API actions for state objects. Update (api/v1/views/__init__.py), to import the newly created file

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """Flask application"""
-from flask import Flask
+from flask import Flask, make_response, jsonify
 import os
 from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 app.register_blueprint(app_views, url_prefix='/api/v1')
 
@@ -14,6 +15,17 @@ app.register_blueprint(app_views, url_prefix='/api/v1')
 def teardown_appcontext(error):
     """Close Storage"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """ 404 Error
+    ---
+    responses:
+      404:
+        description: a resource was not found
+    """
+    return make_response(jsonify({'error': "Not found"}), 404)
 
 
 if __name__ == "__main__":
