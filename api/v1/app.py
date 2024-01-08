b8450428b02cbd4a213375889642add724e23f55
kibuchijw <kibuchijw@gmail.com>
2024-01-08 18:56:33 +0000

    Add (api/v1/app.py), CORS implementation

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,33 +1,36 @@
 #!/usr/bin/python3
-"""
-Python script that starts up a web application
-"""
-
-from os import getenv
+"""Flask application"""
+from flask import Flask, make_response, jsonify
+import os
 from models import storage
 from api.v1.views import app_views
-from flask import Flask, jsonify, make_response
-
+from flask_cors import CORS
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
+app.register_blueprint(app_views, url_prefix='/api/v1')
+cors = CORS(app, resources={r"/api/*": {"origins": "http://0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown(exception):
-    """Method ending the current running
-    SQLAlchemy Session"""
-    return storage.close()
+def teardown_appcontext(error):
+    """Close Storage"""
+    storage.close()
 
 
 @app.errorhandler(404)
 def not_found(error):
-    """Response when client tries to access a route/endpoint/resource
-    that does not exist """
-    return make_response(jsonify({"error": "Not found"}), 404)
+    """ 404 Error
+    ---
+    responses:
+      404:
+        description: a resource was not found
+    """
+    return make_response(jsonify({'error': "Not found"}), 404)
 
 
 if __name__ == "__main__":
-    host = getenv("HBNB_API_HOST") if getenv("HBNB_API_HOST") else "0.0.0.0"
-    port = getenv("HBNB_API_PORT") if getenv("HBNB_API_PORT") else 5000
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.getenv('HBNB_API_PORT', 5000))
     app.run(host=host, port=port, threaded=True)
