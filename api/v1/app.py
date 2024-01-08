958e72f4f3123514ac1a995221c2e124f2ef2cad
Shadkoech <shadrackipruto@gmail.com>
2024-01-08 07:00:26 +0300

    Some stats: Retrieving the number of each objects by type

--- api/v1/app.py
+++ api/v1/app.py
@@ -13,7 +13,6 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 
 
-
 @app.teardown_appcontext
 def teardown(exception):
     """Method ending the current running
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,7 @@ the status of the API"""
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'])
@@ -12,3 +13,15 @@ def status():
     Returns a JSON response with key "status set to OK"
     """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def number_ofObjects():
+    """Retrieves the number of each object type
+    returns key-value pairs where value is the number of objects"""
+    return jsonify(amenities=storage.count("Amenity"),
+                   cities=storage.count("City"),
+                   places=storage.count("Place"),
+                   reviews=storage.count("Review"),
+                   states=storage.count("State"),
+                   users=storage.count("User"))
