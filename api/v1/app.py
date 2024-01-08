889b9859699c8739bce7322b3a04063aaf733069
Shadkoech <shadrackipruto@gmail.com>
2024-01-08 11:42:46 +0300

    Not found: Handling “404 page”

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,34 +1,33 @@
 #!/usr/bin/python3
-"""Flask application"""
-from flask import Flask, make_response, jsonify
-import os
+"""
+Python script that starts up a web application
+"""
+
+from os import getenv
 from models import storage
 from api.v1.views import app_views
+from flask import Flask, jsonify, make_response
+
 
 app = Flask(__name__)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
-
-app.register_blueprint(app_views, url_prefix='/api/v1')
+app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def teardown_appcontext(error):
-    """Close Storage"""
-    storage.close()
+def teardown(exception):
+    """Method ending the current running
+    SQLAlchemy Session"""
+    return storage.close()
 
 
 @app.errorhandler(404)
 def not_found(error):
-    """ 404 Error
-    ---
-    responses:
-      404:
-        description: a resource was not found
-    """
-    return make_response(jsonify({'error': "Not found"}), 404)
+    """Response when client tries to access a route/endpoint/resource
+    that does not exist """
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', 5000))
+    host = getenv("HBNB_API_HOST") if getenv("HBNB_API_HOST") else "0.0.0.0"
+    port = getenv("HBNB_API_PORT") if getenv("HBNB_API_PORT") else 5000
     app.run(host=host, port=port, threaded=True)
